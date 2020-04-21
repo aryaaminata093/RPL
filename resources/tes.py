07a532f97d25899f890b231f23f1c02d0c016304
Pradhipta <pradhiptats@gmail.com>
2020-04-21 11:39:01 +0700

    Revert "api test with python-flask"
    This reverts commit 3e1a4c1388c65501ca34e266cb10b2b462e7a256.

--- resources/tes.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from flask import Response, render_template
-
-
-class tes(Resource):
-	def get(self):
-		return Response(render_template("index.html"),mimetype='text/html')
