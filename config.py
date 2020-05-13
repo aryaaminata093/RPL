ed18efe25c14daa1ea99e25b4a02f790c113ced1
Pradhipta <pradhiptats@gmail.com>
2020-05-13 17:20:44 +0700

    Tambah fitur 'Dokter'

--- config.py
+++ config.py
@@ -7,6 +7,11 @@ host = "localhost:3306"
 dbname = "api"
 JWT_SECRET_KEY = "]WDN{\n\x0bp>R2w)$hZ[DOdD^O^=QM^H'0~"
 
+# user = "dhipzbackup02"
+# password = "mabok_rpl_1337"
+# host ="dhipzbackup02.mysql.pythonanywhere-services.com"
+# dbname = "dhipzbackup02$api_test"
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
