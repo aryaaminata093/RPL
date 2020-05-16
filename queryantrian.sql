f9e08f069612e8938c42d42e70868b9f462e040a
Arya Aminata <60166999+aryaaminata093@users.noreply.github.com>
2020-05-16 11:31:55 +0700

    query antrian

---
+++ queryantrian.sql
@@ -0,0 +1,20 @@
+#declare variabel
+SET @c := '0'; 
+
+#insert data selain noantrian
+INSERT INTO rumahsakit.antrian (tglantrian, id_pasien, id_jadwal) 
+VALUES ('2020-02-20', '1', '22');
+
+#hitung banyak antrian yg sudah ada
+SELECT @c := count(id_pasien) FROM rumahsakit.antrian WHERE id_jadwal='22' AND tglantrian='2020-02-20';
+
+#isi noantrian
+UPDATE rumahsakit.antrian
+SET noantrian = IF(@c='0','1',@c)
+WHERE id_pasien = '1';
+
+#next antrian - delete
+DELETE FROM rumahsakit.antrian
+WHERE id_jadwal='32' AND tglantrian='2020-02-20'
+ORDER BY noantrian
+LIMIT 1;
