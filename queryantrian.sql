58175cf12b11538de19a51836d63cfd325213dcc
Arya Aminata <60166999+aryaaminata093@users.noreply.github.com>
2020-05-19 07:15:07 +0700

    Update queryantrian.sql

--- queryantrian.sql
+++ queryantrian.sql
@@ -3,19 +3,14 @@ FROM rumahsakit.pasien JOIN rumahsakit.antrian ON idpasien=id_pasien
 JOIN rumahsakit.jadwal ON id_jadwal=idjadwal
 JOIN rumahsakit.dokter ON id_dokter=iddokter;
 
-#declare variabel
-SET @c := '0'; 
-
 #insert data selain noantrian
 INSERT INTO rumahsakit.antrian (tglantrian, id_pasien, id_jadwal) 
 VALUES ('2020-02-20', '11', '31');
 
-#hitung banyak antrian yg sudah ada
-SELECT @c := count(id_pasien) FROM rumahsakit.antrian WHERE id_jadwal='31' AND tglantrian='2020-02-20';
-
 #isi noantrian
 UPDATE rumahsakit.antrian
-SET noantrian = IF(@c='0','1',@c)
+SET noantrian = IF((SELECT count(id_pasien) FROM rumahsakit.antrian WHERE id_jadwal='31' AND tglantrian='2020-02-20')
+='0','1',(SELECT count(id_pasien) FROM rumahsakit.antrian WHERE id_jadwal='31' AND tglantrian='2020-02-20'))
 WHERE id_pasien = '11';
 
 #next antrian
