542f8eee51f0adcfc6aaa60224c587b101ba519a
Hafizh Yusuf <55610644+hyusuri@users.noreply.github.com>
2020-05-20 09:47:31 +0700

    Update nextantrian.php

--- public_html/nextantrian.php
+++ public_html/nextantrian.php
@@ -8,7 +8,7 @@ if(isset($_POST['next'])){
 	$sql = "UPDATE dataantrian
 		SET statusantrian = '1'
 		WHERE jadwalid='$idjadwal' AND tglantrian='$tanggal' 
-		AND noantrian=(SELECT min(noantrian) FROM dataantrian WHERE statusantrian='0')";
+		AND noantrian=(SELECT noantrian FROM dataantrian WHERE statusantrian='0' AND jadwalid='$idjadwal' ORDER BY noantrian ASC LIMIT 1)";
 	$query = mysqli_query($konek, $sql);
 
 	if( $query==TRUE ) {
